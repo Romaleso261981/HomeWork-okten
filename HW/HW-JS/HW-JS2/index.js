81180e5f26a00614f1a9f8032f331bf919002ae0
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-04-30 19:39:04 +0300

    did homework №2 on JS

---
+++ HW/HW-JS/HW-JS2/index.js
@@ -0,0 +1,106 @@
+const arr = [1, true, 7, 8, false, 9, "a", "b"];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+//
+
+const obj = {
+    title: "Agata cristie",
+    pageCount: 250,
+    genre: "detective",
+};
+
+const obj2 = {
+    title: "Harry Potter",
+    pageCount: 350,
+    genre: "fantasy",
+};
+const obj3 = {
+    title: "Sherlock Holmes",
+    pageCount: 300,
+    genre: "detective",
+};
+
+const obj4 = {
+    title: "The Lord of the Rings",
+    pageCount: 350,
+    genre: "Detective",
+    authors: ["Agata Cristie", "Arthur Conan Doyle", "J.K. Rowling"],
+}
+const obj5 = {
+    title: "Agata cristie",
+    pageCount: 350,
+    genre: "Detective",
+    authors: ["Agata Cristie", "Arthur Conan Doyle", "J.K. Rowling"],
+}
+const obj6 = {
+    title: "Harry Potter",
+    pageCount: 350,
+    genre: "Fantasy",
+    authors: ["Agata Cristie", "Arthur Conan Doyle", "J.K. Rowling"],
+}
+
+const users = [
+    {
+        id: 1,
+        name: "Vasya",
+        username: "SuperVasya",
+        password: "Luchshiy",
+    },
+    {
+        id: 2,
+        name: "Vasya",
+        username: "SuperVasya",
+        password: "Luchshiy",
+    },
+    {
+        id: 3,
+        name: "Vasya",
+        username: "SuperVasya",
+        password: "Luchshiy",
+    },
+    {
+        id: 4,
+        name: "Vasya",
+        username: "SuperVasya",
+        password: "Luchshiy",
+    },
+    {
+        id: 5,
+        name: "Vasya",
+        username: "SuperVasya",
+        password: "Luchshiy",
+    },
+    {
+        id: 6,
+        name: "Vasya",
+        username: "SuperVasya",
+        password: "Luchshiy",
+    },
+    {
+        id: 7,
+        name: "Vasya",
+        username: "SuperVasya",
+        password: "Luchshiy",
+    },
+    {
+        id: 8,
+        name: "Vasya",
+        username: "SuperVasya",
+        password: "Luchshiy",
+    },
+    {
+        id: 9,
+        name: "Vasya",
+        username: "SuperVasya",
+        password: "Luchshiy",
+    },
+    {
+        id: 10,
+        name: "Vasya",
+        username: "SuperVasya",
+        password: "Luchshiy",
+    },
+
+];
