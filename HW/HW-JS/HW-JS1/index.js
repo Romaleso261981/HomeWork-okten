c09381605acd0b9a2db4d12194966ae7fa66a064
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-04-30 19:26:46 +0300

    did homework №1 on JS

---
+++ HW/HW-JS/HW-JS1/index.js
@@ -0,0 +1,48 @@
+const str = 'hello'
+const str2 = 'owu'
+const str3 = 'com'
+const str4 = 'ua'
+const number = 1
+const number2 = 10
+const number3 = -999
+const number4 = 123
+const number5 = 3.14
+const number6 = 2.7
+const number7 = 16
+const bool = true
+const bool2 = false
+
+console.log(str)
+console.log(str2)
+console.log(str3)
+console.log(str4)
+console.log(number)
+console.log(number2)
+console.log(number3)
+console.log(number4)
+console.log(number5)
+console.log(number6)
+console.log(number7)
+console.log(bool)
+console.log(bool2)
+
+const firstName = "Roma"
+const middleName = "Russinovich"
+const lastName = "Kravchuk"
+
+console.log(firstName + ' ' + middleName + ' ' + lastName)
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log("typeof a", typeof a)
+console.log("typeof b", typeof b)
+console.log("typeof c", typeof c)
+
+
+const firstN = prompt('Enter your name')
+const middleN = prompt('Enter your middleName')
+const lastN = prompt('Enter your lastName')
+
+console.log(firstN + ' ' + middleN + ' ' + lastN)
