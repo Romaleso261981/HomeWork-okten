d00472033225aa27731d69b32b41a35e61fa9247
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-05-04 13:52:34 +0300

    added some code

--- HW/HW-JS/HW-JS5/index.js
+++ HW/HW-JS/HW-JS5/index.js
@@ -98,4 +98,203 @@ console.log(str6.toLowerCase());
 let str = ' dirty string   ';
 console.log(str.trim());
 
+/*- Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+    let str = 'Ревуть воли як ясла повні';
+    let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+*/
 
+function stringToarray(str) {
+    return str.split(' ')
+}
+
+let str7 = 'Ревуть воли як ясла повні';
+console.log(stringToarray(str7));
+
+
+/*
+- є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+*/
+
+let arr = [10, 8, -7, 55, 987, -1011, 0, 1050, 0];
+let arrStr = arr.map(value => value.toString());
+
+console.log(arrStr);
+
+
+/*
+- створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+let nums = [11,21,3];
+sortNums(nums,'ascending') // [3,11,21]
+sortNums(nums,'descending') // [21,11,3]
+*/
+
+function sortNums(arr, direction) {
+    return arr.sort((a, b) => direction === 'ascending' ? a - b : b - a)
+}
+
+console.log(sortNums([11, 21, 3], 'ascending'))
+console.log(sortNums([11, 21, 3], 'descending'))
+
+/*==================================================================================
+- є масив
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+ -- відсортувати його за спаданням за monthDuration
+ -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+ -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+ */
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+console.log(coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration))
+console.log(coursesAndDurationArray.filter((item) => item.monthDuration > 5))
+console.log(coursesAndDurationArray.map((item) => {
+    return {
+        id: coursesAndDurationArray.indexOf(item),
+        title: item.title,
+        monthDuration: item.monthDuration
+    }
+}))
+
+/* описати колоду карт (від 6 до туза без джокерів)*/
+let cards = [];
+let suits = ['spade', 'diamond', 'heart', 'clubs'];
+let values = ['6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
+
+cards = [].concat(...suits.map(suit => values.map(value => ({cardSuit: suit, value: value}))))
+console.log(cards)
+
+/*- знайти піковий туз*/
+console.log("піковий туз", cards.find(card => card.cardSuit === 'spade' && card.value === 'ace'))
+
+/*- всі шістки*/
+console.log("всі шістки", cards.filter(card => card.value === '6'))
+
+/*- всі червоні карти*/
+console.log("всі червоні карти", cards.filter(card => card.cardSuit === 'diamond' || card.cardSuit === 'heart'))
+
+/*- всі буби*/
+console.log("всі буби", cards.filter(card => card.cardSuit === 'diamond'))
+
+/*- всі трефи від 9 та більше*/
+console.log("всі трефи від 9 та більше", cards.filter(card => card.cardSuit === 'clubs' && values.indexOf(card.value) >= 3))
+
+/*==================================================================================*/
+
+/*
+Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
+{
+    spades:[],
+    diamonds:[],
+    hearts:[],
+    clubs:[]
+}
+*/
+
+const cardsObj = cards.reduce((acc, card) => {
+    acc[card.cardSuit].push(card)
+    return acc
+}, {spade: [], diamond: [], heart: [], clubs: []})
+
+console.log("Колода карт", cardsObj)
+
+/*
+взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
+--написати пошук всіх об'єктів, в який в modules є sass
+--написати пошук всіх об'єктів, в який в modules є docker
+*/
+
+const coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+/*
+--написати пошук всіх об'єктів, в який в modules є sass
+*/
+
+console.log("об'єкти, в який в modules є sass", coursesArray.filter(course => course.modules.includes('sass')))
+
+/*
+--написати пошук всіх об'єктів, в який в modules є docker
+ */
+
+console.log("об'єкти, в який в modules є docker", coursesArray.filter(course => course.modules.includes('docker')))
