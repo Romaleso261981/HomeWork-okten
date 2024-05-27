7bfb0e108fe0cf8a122fc2a6fe25e0f1ddd15b97
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-05-27 18:07:01 +0300

    some bag fix

--- HW/HW-JS/HW-JS11/index.js
+++ HW/HW-JS/HW-JS11/index.js
@@ -12,47 +12,198 @@
  */
 
 /*
- Стоврити форму з трьома полями для name,sruname,age та кнопкою. При натисканні на кнопку зчитати данні з полів, та вивести об'єкт в документ. Іншими словами : заповниои форму, натиснули кнопку, під формою з'явився блок з вашим об'єктом
- */
+- взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
+*/
 
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then((value) => {
-    console.log(value.body);
-    return value.json();
-  })
-  .then((value) => {
-    let users = value;
-    for (const user of users) {
-      let div = document.createElement("div");
-      div.innerText = JSON.stringify(user);
-      document.body.appendChild(div);
+fetch("https://dummyjson.com/carts")
+  .then((value) => value.json())
+  .then(({ carts }) => {
+    console.log(carts);
+    return carts.map((cart) => {
+      const cradsWrapper = document.getElementById("carts");
+      cradsWrapper.classList.add("cradsWrapper");
+
+      const div = document.createElement("div");
+      div.classList.add("cartDiv");
+
+      const amountWrapper = document.createElement("div");
+      amountWrapper.classList.add("amountWrapper");
+
+      const id = document.createElement("span");
+      id.classList.add("cardId");
+      id.innerText = `id: ${cart.id}`;
+
+      const discountedTotal = document.createElement("p");
+      discountedTotal.classList.add("discountedTotal");
+      discountedTotal.innerText = `discountedTotal: ${cart.discountedTotal}`;
+
+      const total = document.createElement("p");
+      total.classList.add("total");
+      total.innerText = `total: ${cart.total}`;
+
+      const totalProducts = document.createElement("p");
+      totalProducts.classList.add("totalProducts");
+      totalProducts.innerText = `totalProducts: ${cart.totalProducts}`;
+
+      const totalQuantity = document.createElement("p");
+      totalQuantity.classList.add("totalQuantity");
+      totalQuantity.innerText = `totalQuantity: ${cart.totalQuantity}`;
+
+      const productsWrapper = document.createElement("div");
+      productsWrapper.classList.add("productsWrapper");
+      const productsTitle = document.createElement("h3");
+      productsTitle.className = "productsTitle";
+      productsTitle.innerText = "Products";
+
+      cart.products.map((product) => {
+        const productWrapper = document.createElement("div");
+        productWrapper.classList.add("productWrapper");
+        const productTitle = document.createElement("h4");
+        productTitle.className = "productTitle";
+        productTitle.innerText = product.title;
+
+        const productImage = document.createElement("img");
+        productImage.classList.add("productImage");
+        productImage.src = product.thumbnail;
+
+        productWrapper.appendChild(productTitle);
+        productWrapper.appendChild(productImage);
+
+        productsWrapper.appendChild(productWrapper);
+      });
+
+      amountWrapper.appendChild(discountedTotal);
+      amountWrapper.appendChild(total);
+      amountWrapper.appendChild(totalProducts);
+      amountWrapper.appendChild(totalQuantity);
+
+      div.appendChild(productsWrapper);
+      div.appendChild(amountWrapper);
+
+      cradsWrapper.appendChild(div);
+    });
+  });
+
+/*
+- взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
+*/
+
+fetch("https://dummyjson.com/recipes")
+  .then((value) => value.json())
+  .then(({ recipes }) => {
+    for (const recipe of recipes) {
+      const usersWrapper = document.getElementById("recipes");
+      usersWrapper.classList.add("recipeWrapper");
+
+      const div = document.createElement("div");
+      div.classList.add("recipeDiv");
+
+      const imageWrapper = document.createElement("div");
+      imageWrapper.classList.add("imageWrapper");
+      const image = document.createElement("img");
+      image.src = recipe.image;
+      imageWrapper.appendChild(image);
+
+      const contentWrapper = document.createElement("div");
+      contentWrapper.classList.add("contentWrapper");
+
+      const a = document.createElement("a");
+      a.href = `user-details.html?id=${recipe.id}`;
+      a.innerText = `${recipe.id} ${recipe.name}`;
+
+      const rating = document.createElement("p");
+      rating.classList.add("rating");
+      rating.innerText = `rating: ${recipe.rating}`;
+
+      const ingredientsWrapper = document.createElement("div");
+      ingredientsWrapper.classList.add("ingredientsWrapper");
+      const ingredientsTitle = document.createElement("h3");
+      ingredientsTitle.className = "title";
+      ingredientsTitle.innerText = "Ingredients";
+
+      const ingredients = document.createElement("div");
+      ingredients.classList.add("ingredientsList");
+      recipe.ingredients.map((ingredient) => {
+        const p = document.createElement("p");
+        p.innerText = ingredient;
+        ingredients.appendChild(p);
+      });
+
+      ingredientsWrapper.appendChild(ingredientsTitle);
+      ingredientsWrapper.appendChild(ingredients);
+
+      const instructionsWrapper = document.createElement("div");
+      instructionsWrapper.classList.add("instructionsWrapper");
+      const instructionsTitle = document.createElement("h3");
+      instructionsTitle.className = "title";
+      instructionsTitle.innerText = "Instructions";
+      const instructions = document.createElement("div");
+      instructions.classList.add("instructionsList");
+      recipe.instructions.map((instruction) => {
+        const p = document.createElement("p");
+        p.innerText = instruction;
+        instructions.appendChild(p);
+      });
+
+      instructionsWrapper.appendChild(instructionsTitle);
+      instructionsWrapper.appendChild(instructions);
+
+      contentWrapper.appendChild(rating);
+      contentWrapper.appendChild(instructionsWrapper);
+      contentWrapper.appendChild(ingredientsWrapper);
+      contentWrapper.appendChild(a);
+
+      div.appendChild(imageWrapper);
+      div.appendChild(contentWrapper);
+      usersWrapper.appendChild(div);
     }
   });
 
-let form = document.createElement("form");
-let name = document.createElement("input");
-let surname = document.createElement("input");
-let age = document.createElement("input");
-let button = document.createElement("button");
-let div = document.createElement("div");
+/*
+ Стоврити форму з трьома полями для name,sruname,age та кнопкою. При натисканні на кнопку зчитати данні з полів, та вивести об'єкт в документ. Іншими словами : заповниои форму, натиснули кнопку, під формою з'явився блок з вашим об'єктом
+*/
 
-name.name = "name";
-surname.name = "surname";
-age.name = "age";
-button.innerText = "Send";
+function handleUserDetail(id) {
+  window.location.href = `userDetail.html?id=${id}`;
+  localStorage.setItem("userId", id);
+}
 
-form.appendChild(name);
-form.appendChild(surname);
-form.appendChild(age);
-form.appendChild(button);
-document.body.appendChild(form);
-document.body.appendChild(div);
+function userRender(user) {
+  const usersWrapper = document.getElementById("users");
+  usersWrapper.classList.add("usersWrapper");
 
-button.onclick = () => {
-  let obj = {
-    name: name.value,
-    surname: surname.value,
-    age: age.value
-  };
-  div.innerText = JSON.stringify(obj);
-};
+  const div = document.createElement("div");
+  div.classList.add("userDiv");
+  div.addEventListener("click", () => handleUserDetail(user.id));
+  const a = document.createElement("a");
+  a.href = `user-details.html?id=${user.id}`;
+  a.innerText = `${user.id} ${user.name}`;
+
+  const email = document.createElement("p");
+  email.classList.add("email");
+  email.innerText = user.email;
+
+  const phone = document.createElement("p");
+  phone.classList.add("phone");
+  phone.innerText = user.phone;
+
+  const website = document.createElement("p");
+  website.classList.add("website");
+  website.innerText = user.website;
+
+  div.appendChild(email);
+  div.appendChild(phone);
+  div.appendChild(website);
+
+  div.appendChild(a);
+
+  usersWrapper.appendChild(div);
+}
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((value) => value.json())
+  .then((users) => {
+    for (const user of users) {
+      userRender(user);
+    }
+  });
---
+++ HW/HW-JS/HW-JS11/userDetail.js
@@ -0,0 +1,159 @@
+const renderUserProperties = (
+  elementClasNames,
+  value,
+  label,
+  labelClassNames,
+  rootElement
+) => {
+  const elementWrapper = document.createElement("div");
+  const element = document.createElement("p");
+  const labelElement = document.createElement("span");
+
+  if (labelClassNames && Array.isArray(labelClassNames)) {
+    labelClassNames.map((className) => {
+      labelElement.classList.add(className);
+    });
+  }
+  labelElement.textContent = `${label ?? "label"}:`;
+  if (elementClasNames && Array.isArray(elementClasNames)) {
+    elementClasNames.map((className) => {
+      element.classList.add(className);
+    });
+  }
+  element.textContent = `${value ?? "N/A"}`;
+  elementWrapper.appendChild(labelElement);
+  elementWrapper.appendChild(element);
+  rootElement.appendChild(elementWrapper);
+};
+const createElement = (typeElement, classLists, content) => {
+  const element = document.createElement(typeElement);
+  if (classLists && Array.isArray(classLists)) {
+    classLists.map((classList) => {
+      element.classList.add(classList);
+    });
+  }
+  element.textContent = content;
+  return element;
+};
+const renderPosts = (clasName, comment, index, rootElement) => {
+  const userComment = createElement("div", ["user-comments"], "");
+  userComment.addEventListener("click", () => {
+    localStorage.setItem("currentCommentId", JSON.stringify(comment.id));
+    window.location.href = "user-comment.html";
+  });
+  const element = createElement(
+    "h1",
+    [clasName],
+    `${index + 1}:  ${comment.title ?? "N/A"}`
+  );
+  userComment.appendChild(element);
+  rootElement.appendChild(userComment);
+};
+const findElement = (className, classLists, content) => {
+  let element = document.querySelector(className);
+  if (classLists && Array.isArray(classLists)) {
+    classLists.map((classList) => {
+      element.classList.add(classList);
+    });
+  }
+  if (content) {
+    element.textContent = content;
+  }
+  return element;
+};
+const openPosts = (commentsButton) => {
+  const usersPosts = document.querySelector(".users-posts");
+  usersPosts.classList.toggle("hidden");
+  commentsButton.textContent = "Comments";
+  if (usersPosts.classList.contains("hidden")) {
+    commentsButton.textContent = "Comments";
+  } else {
+    commentsButton.textContent = "Hide";
+  }
+};
+
+const userId = localStorage.getItem("userId");
+
+console.log("userId:", userId);
+
+fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+  .then((value) => value.json())
+  .then((currentUser) => {
+    if (currentUser) {
+      const userDetails = findElement(".user-details", ["user-details"]);
+      const mainTitleWrapper = findElement(".main-title");
+
+      const mainTitle = createElement(
+        "h1",
+        ["main-title"],
+        `User Details: ${currentUser.name}`
+      );
+      mainTitleWrapper.appendChild(mainTitle);
+
+      for (const property in currentUser) {
+        if (
+          typeof currentUser[property] === "object" &&
+          !Array.isArray(currentUser[property])
+        ) {
+          const nestedObject = currentUser[property];
+          for (const nestedProperty in nestedObject) {
+            if (
+              typeof nestedObject[nestedProperty] === "object" &&
+              !Array.isArray(nestedObject[nestedProperty])
+            ) {
+              const deeplyNestedObject = nestedObject[nestedProperty];
+              for (const deeplyNestedProperty in deeplyNestedObject) {
+                renderUserProperties(
+                  ["text", "textColorBlue"],
+                  deeplyNestedObject[deeplyNestedProperty],
+                  `${property}.${nestedProperty}.${deeplyNestedProperty}`,
+                  ["label"],
+                  userDetails
+                );
+              }
+            } else {
+              renderUserProperties(
+                ["text", "textColorGreen"],
+                nestedObject[nestedProperty],
+                `${property}.${nestedProperty}`,
+                ["label"],
+                userDetails
+              );
+            }
+          }
+        } else {
+          renderUserProperties(
+            ["text", "textColorRed"],
+            currentUser[property],
+            property,
+            ["label"],
+            userDetails
+          );
+        }
+      }
+
+      const buttonWrapper = createElement("div", ["button-wrapper"], "");
+
+      const commentsButton = createElement(
+        "button",
+        ["userDetail-button"],
+        "Comments"
+      );
+      commentsButton.addEventListener("click", () => {
+        openPosts(commentsButton);
+      });
+
+      const backButton = createElement("button", ["userDetail-button"], "Back");
+      backButton.addEventListener("click", () => {
+        window.location.href = "index.html";
+      });
+
+      buttonWrapper.appendChild(backButton);
+      buttonWrapper.appendChild(commentsButton);
+
+      userDetails.appendChild(buttonWrapper);
+    } else {
+      console.error("Current user data not found.");
+    }
+  })
+  .catch((error) => console.error("Error fetching current user:", error));
